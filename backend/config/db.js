bb0201a96a5591ec56aee2a0b46b75173d1857af
EsraaKrayym <153622145+EsraaKrayym@users.noreply.github.com>
2026-06-10 11:55:15 +0200

    Initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+//const pool = new Pool({
+   // user: "postgres",
+   // host: "localhost",
+ //   database: "infrastructure_db",
+  //  password: "saso.hell835",
+   // port: 5432,
+//});
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "infrastructure_db",
-    password: "saso.hell835",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 pool.connect()
     .then(() => console.log("PostgreSQL connected ✅"))
     .catch(err => console.error("PostgreSQL error ❌", err));
